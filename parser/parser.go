828fd744d430d3c1c7a438df739385fb39cbca57
Leo <leogutierrezramirez@gmail.com>
2018-09-22 18:04:31 -0600

    preparing parser

--- parser/parser.go
+++ parser/parser.go
@@ -103,6 +103,16 @@ func (p *Parser) parseInfixExpression(left ast.Expression) ast.Expression {
 
 	precedence := p.curPrecedence()
 	p.nextToken()
+
+	/*
+			For having + right associative:
+			if expression.Operator == "+" {
+		        expression.Right = p.parseExpression(precedence - 1)
+		    } else {
+		        expression.Right = p.parseExpression(precedence)
+			}
+	*/
+
 	expression.Right = p.parseExpression(precedence)
 
 	return expression
