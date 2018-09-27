7b41754f016cb77dfb804520735be9fa9bd25fa4
leogtzr <leogutierrezramirez@gmail.com>
2018-09-26 22:09:22 -0600

    adding max function

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -18,4 +18,36 @@ var builtins = map[string]*object.Builtin{
 			}
 		},
 	},
+
+	"max": &object.Builtin{
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) < 1 {
+				return newError("wrong number of arguments. got=%d, want=1", len(args))
+			}
+
+			max, ok := args[0].(*object.Integer)
+			if !ok {
+				return newError("arguments to `max` not supported, got %s", args[0].Type())
+			}
+
+			for _, arg := range args {
+				n, ok := arg.(*object.Integer)
+				if !ok {
+					return newError("argument to `max` not supported, got %s", arg.Type())
+				}
+				if n.Value > max.Value {
+					max = n
+				}
+			}
+			return &object.Integer{Value: max.Value}
+
+			// switch arg := args[0].(type) {
+			// case *object.Integer:
+			// 	return &object.Integer{Value: int64()}
+			// default:
+			// 	return newError("argument to `len` not supported, got %s",
+			// 		args[0].Type())
+			// }
+		},
+	},
 }
